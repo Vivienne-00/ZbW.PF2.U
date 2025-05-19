826bab8c9ca12416d72e53bdd41e92aed79ca272
Mesaros, Thomas <thomas.mesaros@unisg.ch>
2025-05-19 19:09:31 +0200

    Bubble Sort naive implementation

--- MB15/SortComparison/SortingAlgorithms/BubbleSort.cs
+++ MB15/SortComparison/SortingAlgorithms/BubbleSort.cs
@@ -7,14 +7,18 @@ namespace MB15.SortComparison.SortingAlgorithms
         public override string Name => "Bubblesort";
         public override void Sort(IList<int> arrayToSort)
         {
-
-
-
+          for (int i = 0; i < arrayToSort.Count; i++)
+          {
+            for (int j = i; j < arrayToSort.Count - (1 + i); j++)
+            {
+              if (arrayToSort[j] > arrayToSort[j + 1])
+              {
+                var temp = arrayToSort[j];
+                arrayToSort[j] = arrayToSort[j + 1];
+                arrayToSort[j + 1] = temp;
+              }
+            }
+          }
         }
-
-        private void SwapItems(IList<int> arrayToSort, int index1, int index2)
-        {
-
-        }
+      }
     }
-}
